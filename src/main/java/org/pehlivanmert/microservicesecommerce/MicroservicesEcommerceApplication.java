68b324c3c465eb2daba974c969ba715552dde3eb
Mert Pehlivan <pehlivannmert@gmail.com>
2025-04-14 23:21:19 +0300

    initialize microservices-ecommerce project with Gradle and Spring Boot setup

---
+++ src/main/java/org/pehlivanmert/microservicesecommerce/MicroservicesEcommerceApplication.java
@@ -0,0 +1,13 @@
+package org.pehlivanmert.microservicesecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesEcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/pehlivanmert/microservicesecommerce/MicroservicesEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pehlivanmert.microservicesecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
